b81e8916d751783356dee69abf8e9005a46e2f21
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2023-12-15 14:37:26 +0700

    Lab 9_2 Updated

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,7 +1,19 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int x,int y){
+    if(x<=0 || y<=0){
+        cout<<"Invalid input";
+    }
+    else{
+    for(int i=0; i<x; i++){
+        for(int j=0; j<y; j++){
+            cout << "O";
+        }
+        cout << endl;
+    }
+    }
+}
 
 int main(){
 	
